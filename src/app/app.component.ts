c3b12a12b5bcabf8003bd6d79973a30fe2e69746
Joe Zabinski <jzabinski@dolios.dev>
2022-06-02 12:42:44 -0400

    Use webworker

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { fromWorker } from 'observable-webworker';
+import { Observable, of } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'obsvbl-webworker';
+
+  runWorker() {
+    const input$: Observable<string> = of('hello');
+
+    fromWorker<string, string>(
+      () => new Worker(new URL('./app.worker', import.meta.url), { type: 'module' }),
+      input$
+    ).subscribe((message) => {
+      console.log(`Got message`, message);
+    });
+  }
 }
